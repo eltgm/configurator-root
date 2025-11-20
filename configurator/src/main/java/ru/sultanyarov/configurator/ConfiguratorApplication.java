aa9a4472e0762a1161a3c7da8b7665ab24ab3497
v.sultanyarov <v.sultanyarov@tinkoff.ru>
2025-11-20 17:36:30 +0300

    Init

--- configurator/src/main/java/ru/sultanyarov/configurator/ConfiguratorApplication.java
+++ configurator/src/main/java/ru/sultanyarov/configurator/ConfiguratorApplication.java
@@ -2,8 +2,10 @@ package ru.sultanyarov.configurator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class ConfiguratorApplication {
 
     public static void main(String[] args) {
